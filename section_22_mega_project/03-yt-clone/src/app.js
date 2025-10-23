9e08822e1f623fc4a2c494841f0fe3d06659fb55
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-10-23 18:52:16 +0530

    add healthcheck route and controller

--- section_22_mega_project/03-yt-clone/src/app.js
+++ section_22_mega_project/03-yt-clone/src/app.js
@@ -14,4 +14,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// routes
+import healthcheckRouter from "./routes/healthcheck.route.js";
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+
 export default app;
