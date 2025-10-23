22dd037b7329c8c2c8543b9602333e012dc86780
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-10-23 21:44:43 +0530

    add user model methods for password hashing and JWT token generation

--- section_22_mega_project/03-yt-clone/src/models/user.model.js
+++ section_22_mega_project/03-yt-clone/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -47,5 +49,36 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.method.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, email: this.email, username: this.username },
+    process.env.ACCRESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCRESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.method.generateRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id, email: this.email, username: this.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
